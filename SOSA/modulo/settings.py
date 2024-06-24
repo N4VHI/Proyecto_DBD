6ec2d8547298934e4e3cf5a6f50693e0ee853abb
ChrisTw0 <tekimuay@hotmail.com>
2024-06-24 11:27:05 -0500

    Megaactu

--- SOSA/modulo/settings.py
+++ SOSA/modulo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'modulo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'RRHH',
+        'USER': 'postgres',
+        'PASSWORD': 'renatososa',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -119,6 +123,9 @@ USE_TZ = True
 # settings.py
 
 ...
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 
--- SOSA/modulo/urls.py
+++ SOSA/modulo/urls.py
@@ -17,15 +17,29 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from tasks import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('registrar/', views.registrar_postulante, name='registrar_postulante'),
+    path('success/', views.success_view, name='success'),
+    path('postulantes/', views.listar_postulantes, name='listar_postulantes'),
+    path('postulantes/<int:id_cand>/', views.detalle_postulante, name='detalle_postulante'),
     path('seleccionar/', views.seleccionar_horario_puesto, name='seleccionar_horario_puesto'),
+    path('seleccionar_vacante/', views.seleccionar_vacante, name='seleccionar_vacante'),
     path('preseleccion/', views.preseleccion_candidatos, name='preseleccion_candidatos'),
     path('seleccion_final/', views.seleccion_final, name='seleccion_final'),
-    path('crear_perfil/', views.crear_perfil, name='crear_perfil'),
+    path('crear_vacante/', views.crear_vacante, name='crear_vacante'),
+    path('listar_vacantes/', views.listar_vacantes, name='listar_vacantes'),
     path('programar_entrevista/', views.programar_entrevista, name='programar_entrevista'),
+    path('listar_entrevistas/', views.listar_entrevistas, name='listar_entrevistas'),
     path('actualizar_evaluacion/', views.actualizar_evaluacion, name='actualizar_evaluacion'),
+    path('listado_seleccionados/', views.listado_seleccionados, name='listado_seleccionados'),
+    path('listar_evaluaciones/', views.listar_evaluaciones, name='listar_evaluaciones'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
